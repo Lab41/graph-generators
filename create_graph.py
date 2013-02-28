e20b7dd26be1ca6d1de2cf33bb7c476016f9a578
Charlie Lewis <clewis@iqt.org>
2013-02-28 11:21:05 -0800

    cleaned up output, started fleshing out functions to write to graph file

--- create_graph.py
+++ create_graph.py
@@ -1,43 +1,72 @@
 #!/usr/bin/python
 
-import sys
+import sys, uuid
 
-def header(type):
-    # !! TODO
-    print "header"
+def output_file(file):
+    fo = open(file, 'w')
+    return fo
 
-def is_directed(type, directed):
-    # !! TODO
-    print "directed or undirected"
+def header(type, node_attrs, edge_attrs, fo):
+    node_attrs_a = []
+    edge_attrs_a = []
+    if type == "graphml":
+        header = "<?xml version=\"1.0\" ?>\n<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\">\n"
+        fo.write(header)
 
-def create_node(type, num_nodes, node_attrs):
+        # !! TODO add keys for attr types for nodes and edges
+
+    return node_attrs_a, edge_attrs_a
+
+def is_directed(type, directed, fo):
+    graph_id = str(uuid.uuid4())
+    directed_str = "undirected" 
+    if directed == 1:
+        directed_str = "directed"
+
+    if type == "graphml":
+        graph_str = "    <graph id=\""+graph_id+"\" edgedefault=\""+directed_str+"\">\n"
+        fo.write(graph_str)
+
+    return graph_id
+
+def create_node(type, num_nodes, node_attrs, fo):
     # !! TODO
     print "node"
 
-def create_edge(type, min, max, mini, mino, maxi, maxo, edge_attrs):
+def create_edge(type, min, max, mini, mino, maxi, maxo, edge_attrs, fo):
     # !! TODO
     print "edge"
 
-def close_graph(type):
-    # !! TODO
-    print "close graph"
+def close_graph(type, fo):
+    closer = ""
+    if type == "graphml":
+        closer = "    </graph>\n</graphml>"
+    fo.write(closer)
+    fo.close()
 
-def generate_graph(type):
-    # !! TODO
-    print "generate graph"
+def generate_graph(type, file, num_nodes, directed, node_attrs, edge_attrs, min, max, mini, mino, maxi, maxo):
+    fo = output_file(file)
+    node_attrs_a, edge_attrs_a = header(type, node_attrs, edge_attrs, fo)
+    graph_id = is_directed(type, directed, fo)
+
+    # !! TODO node creation
+    # !! TODO edge creation
+
+    close_graph(type, fo)
 
 def print_help():
-    print "-n <num of nodes> (default is 1000)"
-    print "-max <max degree of nodes> (only used with undirected, default is 10)"
-    print "-min <min degree of nodes> (only used with undirected, default is 1)"
-    print "-maxi <max in degree of nodes> (only used with directed flag, default is 10)"
-    print "-maxo <max out degree of nodes> (only used with directed flag, default is 10)"
-    print "-mini <min in degree of nodes> (only used with directed flag, default is 1)"
-    print "-mino <min out degree of nodes> (only used with directed flag, default is 1)"
-    print "-na <num of node attributes> (default is 2)"
-    print "-ea <num of edge attributes> (default is 0)"
-    print "-d (directed, undirected by default)"
-    print "-t <output type> (graphml by default, options include gml and graphson)"
+    print "\n-n <num of nodes> (default is 1000)\n"
+    print "-max <max degree of nodes> (only used with undirected, default is 10)\n"
+    print "-min <min degree of nodes> (only used with undirected, default is 1)\n"
+    print "-maxi <max in degree of nodes> (only used with directed flag, default is 10)\n"
+    print "-maxo <max out degree of nodes> (only used with directed flag, default is 10)\n"
+    print "-mini <min in degree of nodes> (only used with directed flag, default is 1)\n"
+    print "-mino <min out degree of nodes> (only used with directed flag, default is 1)\n"
+    print "-na <num of node attributes> (default is 2)\n"
+    print "-ea <num of edge attributes> (default is 0)\n"
+    print "-d (directed, undirected by default)\n"
+    print "-t <output type> (graphml by default, options include gml and graphson)\n"
+    print "-o <path to output file> (default is 'graph')\n"
     sys.exit(0)
     
 def check_directed(directed, arg, value, flag):
@@ -63,6 +92,7 @@ def process_args(args):
     edge_attrs = 0
     directed_num = -1
     type = "graphml"
+    output = "graph"
 
     # process whether it is directed or not
     i = 0
@@ -123,6 +153,13 @@ def process_args(args):
                 type = args[i+1]
             except:
                 print_help()
+        elif args[i] == "-o":
+            try:
+                output = args[i+1]
+                f = open(output, 'w')
+                f.close()
+            except:
+                print_help()
         else:
             print_help()
         i += 2
@@ -131,7 +168,7 @@ def process_args(args):
     if type != "graphml" and type != "gml" and type != "graphson":
         print_help()
 
-    return type, num_nodes, directed, min, max, mini, mino, maxi, maxo, node_attrs, edge_attrs
+    return type, output, num_nodes, directed, min, max, mini, mino, maxi, maxo, node_attrs, edge_attrs
 
 def get_args():
     args = []
@@ -141,5 +178,22 @@ def get_args():
 
 if __name__ == "__main__":
     args = get_args()
-    type, num_nodes, directed, min, max, mini, mino, maxi, maxo, node_attrs, edge_attrs = process_args(args)
-    print type, num_nodes, directed, min, max, mini, mino, maxi, maxo, node_attrs, edge_attrs
+    type, output, num_nodes, directed, min, max, mini, mino, maxi, maxo, node_attrs, edge_attrs = process_args(args)
+    print "Generating the following graph:"
+    print "\tType: \t\t\t",type
+    print "\tOutput File: \t\t",output
+    print "\tNodes: \t\t\t",num_nodes
+    if directed == 0:
+        print "\tDirected: \t\tNo"
+        print "\tMinimum Degree: \t",min
+        print "\tMaximum Degree: \t",max
+    else:
+        print "\tDirected: \t\tYes"
+        print "\tMinimum In Degree: \t",mini
+        print "\tMaximum In Degree: \t",maxi
+        print "\tMinimum Out Degree: \t",mino
+        print "\tMaximum Out Degree: \t",maxo
+    print "\tNode Attributes: \t",node_attrs
+    print "\tEdge Attributes: \t",edge_attrs 
+    
+    generate_graph(type, output, num_nodes, directed, node_attrs, edge_attrs, min, max, mini, mino, maxi, maxo)
